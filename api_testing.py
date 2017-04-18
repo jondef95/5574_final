dbc779f7cfaa11a3d9f88977a7163f646a384e1e
Jonathan <jondef95@vt.edu>
2017-04-17 22:14:56 -0400

    working internal code to generate facts; writing tests

---
+++ api_testing.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestInternalMethods(unittest.TestCase):
+
+	def test_something(self):
+		""" testing some stuff """
+		self.assertTrue(True)
+
+
+if __name__ == '__main__':
+	unittest.main()
